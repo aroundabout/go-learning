13bd35df32b8356269c345b2e6f1ee90645b6cf8
Dandelion <kainanyaoo@gmail.com>
2025-03-26 23:17:54 +0800

    "feat: scan tcp port"

---
+++ example/tcp_scan_test.go
@@ -0,0 +1,46 @@
+package example
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"sync"
+	"testing"
+	"time"
+)
+
+func isOpen(host string, port int, timeout time.Duration) bool {
+	time.Sleep(time.Millisecond * 1)
+	conn, err := net.DialTimeout("tcp", fmt.Sprintf("%s:%d", host, port), timeout)
+	if err == nil {
+		_ = conn.Close()
+		return true
+	}
+
+	return false
+}
+
+func TestTcpScan(t *testing.T) {
+	hostname := flag.String("hostname", "google.com", "hostname to test")
+	startPort := flag.Int("start-port", 80, "the port on which the scanning starts")
+	endPort := flag.Int("end-port", 10000, "the port from which the scanning ends")
+	timeout := flag.Duration("timeout", time.Millisecond*200, "timeout")
+	flag.Parse()
+
+	ports := []int{}
+
+	wg := &sync.WaitGroup{}
+	for port := *startPort; port <= *endPort; port++ {
+		wg.Add(1)
+		go func(p int) {
+			opened := isOpen(*hostname, p, *timeout)
+			if opened {
+				ports = append(ports, p)
+			}
+			wg.Done()
+		}(port)
+	}
+
+	wg.Wait()
+	fmt.Printf("opened ports: %v\n", ports)
+}
